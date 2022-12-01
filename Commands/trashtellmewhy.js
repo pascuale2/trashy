94b838b8fce00c853cd238bf1269e08b9e143b6a
Erwin Pascual <epas97@hotmail.com>
2022-12-01 13:52:13 -0700

    Fix trash code

--- Commands/trashtellmewhy.js
+++ Commands/trashtellmewhy.js
@@ -1,6 +1,7 @@
 const voice = require("@discordjs/voice");
 
 class TrashTellMeWhy{
+    lastIndex = 0;
     soundPaths = [
         "Assets/heartache.m4a",
         "Assets/mistake.m4a",
@@ -9,8 +10,14 @@ class TrashTellMeWhy{
 
     run(client, message, args) {
         const player = require('../Utility/playvoice');
-        const randomIndex = Math.floor(Math.random() * this.soundPaths.length);
-        player.Play(message, voice.createAudioResource(this.soundPaths[randomIndex]));
+        do {
+            const randomIndex = Math.floor(Math.random() * this.soundPaths.length);
+            if (randomIndex != this.lastIndex) {
+                this.lastIndex = randomIndex;
+                break;
+            }
+        } while (this.soundPaths.length > 1);
+        player.Play(message, voice.createAudioResource(this.soundPaths[this.lastIndex]));
     }
 
     config = {
